86fec1bbeeb02a7ad4fd3df7bd203d021dc97cdd
Pallavi <pallavibu44@gmail.com>
2023-06-09 18:17:54 +0530

    added

--- my-card/src/App.js
+++ my-card/src/App.js
@@ -1,11 +1,25 @@
 import React from "react"
 import Card from "./card"
+import { useState } from "react"
+import Axios from "axios"
 
 function App(){
+
+    const [details, setDetails] = useState({})
+
+    const fetechDeatils = async() => {
+        const {data} = await Axios.get('https://randomuser.me/api');
+    
+        console.log("RESPONSE", data);
+    };
+    let list = ["Apple", "Bat", "Cat"]
     return(
         <>
          <div>App</div>
-         <Card />
+         <Card myname="Pallavi" list={list} />
+         <button
+         onClick={fetechDeatils}
+         >get details</button>
         </>
     )
 }
--- my-card/src/card.jsx
+++ my-card/src/card.jsx
@@ -1,9 +1,18 @@
 import React from "react"
 
-function Card(){
+function Card({myname, list}){
+    //console.log(props)
     return(
-        <div>This is a card</div>
-    )
+        <>
+            <div>This is a card</div>
+            <div>{myname}</div>
+            <ul>
+                {list.map((l) => (
+                    <li key={l}>{l}</li> 
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default Card;
