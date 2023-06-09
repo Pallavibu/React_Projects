4c70c2a50a00d0e0b8f1129ebab166aee65d2f01
Pallavi <pallavibu44@gmail.com>
2023-06-09 15:17:33 +0530

    working on my-card

---
+++ my-card/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Card from "./card"
+
+function App(){
+    return(
+        <>
+         <div>App</div>
+         <Card />
+        </>
+    )
+}
+export default App;
