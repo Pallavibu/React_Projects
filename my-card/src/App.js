76d0054cf4082e7153331b1b6cf05243657918c4
Pallavi <pallavibu44@gmail.com>
2023-06-12 11:00:03 +0530

    main

--- my-card/src/App.js
+++ my-card/src/App.js
@@ -11,12 +11,15 @@ function App(){
         const {data} = await Axios.get('https://randomuser.me/api');
     
         console.log("RESPONSE", data);
+
+        const details = data.results[0];
+        setDetails(details)
     };
     let list = ["Apple", "Bat", "Cat"]
     return(
         <>
          <div>App</div>
-         <Card myname="Pallavi" list={list} />
+         <Card myname="Pallavi" list={list} details={details} />
          <button
          onClick={fetechDeatils}
          >get details</button>
