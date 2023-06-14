a35ec16b4e3c96f3ad58502c8d6103056a8422cd
Pallavi <pallavibu44@gmail.com>
2023-06-14 06:51:06 +0530

    updated

--- my-card/src/App.js
+++ my-card/src/App.js
@@ -1,29 +1,36 @@
-import React from "react"
-import Card from "./card"
-import { useState } from "react"
-import Axios from "axios"
+import React, { useEffect } from "react";
+import Card from "./card";
+import { useState } from "react";
+import Axios from "axios";
 
-function App(){
+function App() {
+  const [details, setDetails] = useState({});
 
-    const [details, setDetails] = useState({})
+  const fetechDetails = async () => {
+    const { data } = await Axios.get("https://randomuser.me/api");
 
-    const fetechDeatils = async() => {
-        const {data} = await Axios.get('https://randomuser.me/api');
-    
-        console.log("RESPONSE", data);
+    console.log("RESPONSE", data);
 
-        const details = data.results[0];
-        setDetails(details)
-    };
-    let list = ["Apple", "Bat", "Cat"]
-    return(
-        <>
-         <div>App</div>
-         <Card myname="Pallavi" list={list} details={details} />
-         <button
-         onClick={fetechDeatils}
-         >get details</button>
-        </>
-    )
+    const details = data.results[0];
+    setDetails(details);
+  };
+  let list = ["Anurag", "Bharat", "Chetan"];
+
+  useEffect(() => {
+    fetechDetails();
+  }, []);
+  return (
+    <>
+      <div>App</div>
+      <Card myname="Pallavi" list={list} details={details} />
+      <button onClick={fetechDetails}>get details</button>
+    </>
+  );
 }
 export default App;
+
+
+
+// practice debouncing
+// practice more and more hooks (important : useState() and useEffect())
+// fetch free apis and make projects
--- my-card/src/card.jsx
+++ my-card/src/card.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 
-function Card({myname, list}){
+function Card({myname, list, details}){
     //console.log(props)
     return(
         <>
             <div>This is a card</div>
             <div>{myname}</div>
+            <h1>{details.gender}</h1>
+            <h1>{details.name?.first}</h1>
             <ul>
                 {list.map((l) => (
                     <li key={l}>{l}</li> 
