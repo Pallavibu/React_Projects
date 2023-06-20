d64328467e21d484ac8f5b4ab74289bf855f4818
Pallavi <pallavibu44@gmail.com>
2023-06-20 11:28:30 +0530

    done

--- practice/src/Components/Navbar.js
+++ practice/src/Components/Navbar.js
@@ -1,6 +1,17 @@
+import react,{ useState } from 'react';
 import Cards from './Cards';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increament = () =>{
+    setCount(count + 1)
+  }
+
+  const decreament = () =>{
+    setCount(count-1)
+  }
   return (
     <>
     <h1 className="text-green-800 font-bold ">Hello World</h1>
@@ -8,6 +19,12 @@ function App() {
     <Cards name="Apple"/>
     <Cards name="ball"/>
     <Cards name="cat"/>
+    <h1>Counter App</h1>
+    <div>{count}</div>
+    <div className='button'>
+        <button onClick={increament}>+</button>
+        <button onClick={decreament}>-</button>
+    </div>
     </>
   );
 }
